64aa2ce83a8eb0875e7dc432fbe06b40434b7043
Sun Haihua <1658476813@qq.com>
2018-11-29 20:22:59 +0800

    新建登录分支-组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Login from '../commponents/login/login.vue'
+import Login from '@/commponents/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
