d95c1d5a49391202b84750e5db341ea439446f58
Sun Haihua <1658476813@qq.com>
2018-11-30 11:28:37 +0800

    首页布局完成

--- src/commponents/home/home.vue
+++ src/commponents/home/home.vue
@@ -4,21 +4,93 @@
       <el-row>
         <el-col :span="4">
           <div class="grid-content bg-purple">
-              <img src="@/assets/images/logo.png" alt="">
+            <img src="@/assets/images/logo.png" alt>
           </div>
         </el-col>
         <el-col :span="18" class="admin">
-              <h3>电商后台管理系统</h3>
+          <h3>电商后台管理系统</h3>
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-              <a href=""  class="logout">退出</a>
+            <a href class="logout">退出</a>
           </div>
         </el-col>
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <!-- 左侧 -->
+      <el-aside class="aside" width="200px">
+        <el-menu :unique-opened="true">
+          <el-submenu index="1">
+            <!-- 1 -->
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+        <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+        <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+        <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>选项1</span>
+            </el-menu-item>
+          </el-submenu>
+        <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-setting"></i>
+              <span>选项1</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -43,11 +115,11 @@ export default {};
 }
 
 /* 头部样式 */
-.admin{
-    text-align: center;
+.admin {
+  text-align: center;
 }
 .logout {
-    line-height: 60px;
-    text-decoration: none;
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
