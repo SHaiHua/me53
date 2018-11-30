2cffd6abbf022af07c57aba2c24a9c90796865db
Sun Haihua <1658476813@qq.com>
2018-11-30 10:53:14 +0800

    头部样式调整

--- src/commponents/home/home.vue
+++ src/commponents/home/home.vue
@@ -1,13 +1,53 @@
 <template>
-    <div>home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+              <img src="@/assets/images/logo.png" alt="">
+          </div>
+        </el-col>
+        <el-col :span="18" class="admin">
+              <h3>电商后台管理系统</h3>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+              <a href=""  class="logout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+}
 
+/* 头部样式 */
+.admin{
+    text-align: center;
+}
+.logout {
+    line-height: 60px;
+    text-decoration: none;
+}
 </style>
--- src/commponents/login/login.vue
+++ src/commponents/login/login.vue
@@ -34,6 +34,8 @@ export default {
           meta: { msg, status }
         } = res.data;
         if (status === 200) {
+          // 保存token
+          localStorage.setItem('token',data.token)
           this.$router.push({ name: "home" });
           this.$message.success(msg);
         } else {
